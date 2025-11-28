249fcfe802bb1f64279139087fb0a6882fd86a9c
Peilei Ye <peileiye@Peileis-MacBook-Air.local>
2025-11-28 10:04:43 +0100

    añadido envase, cambiando getTodosProductos y getProductoById

--- server/src/services/productos.service.js
+++ server/src/services/productos.service.js
@@ -1,9 +1,9 @@
 const pool = require("../config/db");
 
 module.exports = {
-  async getTodosProductos() {
-    const [rows] = await pool.query(`
-      SELECT 
+    async getTodosProductos() {
+        const [rows] = await pool.query(`
+      SELECT
         p.id_producto,
         p.referencia,
         p.nombre,
@@ -12,41 +12,57 @@ module.exports = {
         p.url_imagen,
         p.destacado,
         p.disponible,
+
         c.nombre AS categoria,
         g.nombre AS grupo,
-        o.nombre AS origen
+        o.nombre AS origen,
+
+        GROUP_CONCAT(DISTINCT e.descripcion SEPARATOR ' || ') AS envases
+
       FROM productos p
       LEFT JOIN categorias c ON p.id_categoria = c.id_categoria
       LEFT JOIN grupos g ON p.id_grupo = g.id_grupo
       LEFT JOIN origenes o ON p.id_origen = o.id_origen
-      ORDER BY p.id_producto ASC
+      LEFT JOIN productos_envases pe ON p.id_producto = pe.id_producto
+      LEFT JOIN envases e ON pe.id_envase = e.id_envase
+      GROUP BY p.id_producto
+      ORDER BY p.id_producto ASC;
     `);
 
-    return rows;
-  },
+        return rows;
+    },
 
-  async getProductoById(id) {
-    const [rows] = await pool.query(`
-      SELECT 
-        p.id_producto,
-        p.referencia,
-        p.nombre,
-        p.nombre_ingles,
-        p.descripcion,
-        p.url_imagen,
-        p.destacado,
-        p.disponible,
-        c.nombre AS categoria,
-        g.nombre AS grupo,
-        o.nombre AS origen
-      FROM productos p
-      LEFT JOIN categorias c ON p.id_categoria = c.id_categoria
-      LEFT JOIN grupos g ON p.id_grupo = g.id_grupo
-      LEFT JOIN origenes o ON p.id_origen = o.id_origen
-      WHERE p.id_producto = ?
-      LIMIT 1
-    `, [id]);
+    async getProductoById(id) {
+        const [rows] = await pool.query(`
+    SELECT
+      p.id_producto,
+      p.referencia,
+      p.nombre,
+      p.nombre_ingles,
+      p.descripcion,
+      p.url_imagen,
+      p.destacado,
+      p.disponible,
 
-    return rows[0] || null;
-  }
+      c.nombre AS categoria,
+      g.nombre AS grupo,
+      o.nombre AS origen,
+
+      GROUP_CONCAT(DISTINCT e.descripcion SEPARATOR ' || ') AS envases
+
+    FROM productos p
+    LEFT JOIN categorias c ON p.id_categoria = c.id_categoria
+    LEFT JOIN grupos g ON p.id_grupo = g.id_grupo
+    LEFT JOIN origenes o ON p.id_origen = o.id_origen
+    LEFT JOIN productos_envases pe ON p.id_producto = pe.id_producto
+    LEFT JOIN envases e ON pe.id_envase = e.id_envase
+    WHERE p.id_producto = ?
+    GROUP BY p.id_producto;
+  `, [id]);
+
+        return rows[0];
+    }
+
+
 };
+
