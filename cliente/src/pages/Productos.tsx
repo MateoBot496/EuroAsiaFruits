c6af8f24c49f27ea647b92795deb235c55dcb35b
MateoBot496 <mateomontesacosta@gmail.com>
2025-11-29 18:01:48 +0100

    Añadido productos, y parte del front

---
+++ cliente/src/pages/Productos.tsx
@@ -0,0 +1,27 @@
+import type { JSX } from "react";
+import useProductos from "../hooks/useProductos";
+import ProductoCard from "../components/ProductoCard";
+
+function Productos(): JSX.Element {
+    const {productos, loading} = useProductos();
+    return (
+        <>
+
+            <div className="productos">
+                
+                
+                <h1 className="text-3xl font-bold underline "> Hola mundillo desde Productos </h1>
+                <div className="productos-content">
+                    {productos.map((producto) => (
+                        <ProductoCard key={producto.id_producto} producto={producto} />
+                        )
+                    )}
+                    
+                </div>
+                
+            </div>
+        </>
+    )
+
+}
+export default Productos;
