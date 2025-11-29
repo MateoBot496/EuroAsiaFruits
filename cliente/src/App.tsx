d81bc449f6897c1a46624f73d5b4a385060bc785
MateoBot496 <mateomontesacosta@gmail.com>
2025-11-29 12:55:17 +0100

    Cambios varios

--- cliente/src/App.tsx
+++ cliente/src/App.tsx
@@ -1,9 +1,38 @@
-import { useState, type JSX } from 'react'
+import { useEffect, useState, type JSX } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 
+interface Producto {
+  id: number
+  nombre: string
+  descripcion: string
+  precio: number
+  // añade más propiedades si tu API devuelve más
+}
+
 function App() : JSX.Element {
-  const [count, setCount] = useState(0)
+  const [productos, setProductos] = useState<Producto[]>([]);
+
+  useEffect(() => {
+    const fetchProductos = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/public/productos/");
+
+        if (!res.ok) {
+          throw new Error(`Error del servidor: ${res.status}`);
+        }
+
+        const data: Producto[] = await res.json();
+        setProductos(data);
+        console.log(productos);
+
+      } catch (err){
+        console.log(err);
+      }
+    }
+
+    fetchProductos();
+  }, [])
 
   return (
     <>
