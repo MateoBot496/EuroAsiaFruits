68f96b019ad33cb69861744809cadc93e6b284ad
MateoBot496 <mateomontesacosta@gmail.com>
2025-11-30 15:14:22 +0100

    Inicio del home

--- cliente/src/components/Navbar.tsx
+++ cliente/src/components/Navbar.tsx
@@ -1,12 +1,34 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
 export default function Navbar() {
   return (
-    <nav className="w-full b-2 border-black bg-blue-100 p-4 gap-4 flex justify-around items-center">
-      <h1 className="text-2xl font-semibold">Navbar</h1>
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="logo" />
 
       <Link to="/" className="navLink">Home</Link>
       <Link to="/productos" className="navLink">Productos</Link>
       <Link to="/about"  className="navLink">About</Link>
+      
+      
+      <div className="searchbar">
+
+        <input
+          className="searchbar__input"
+          type="text"
+          placeholder="Buscar productos..."
+        />
+
+        <button className="searchbar__icon-right" aria-label="Buscar">
+          
+          <svg viewBox="0 0 24 24" aria-hidden="true" className="w-6">
+            <circle cx="11" cy="11" r="7" stroke="currentColor" strokeWidth="2" fill="none"/>
+            <path d="M20 20l-4-4" stroke="currentColor" strokeWidth="2"
+                  strokeLinecap="round"/>
+          </svg>
+        </button>
+      </div>
+
     </nav>
   );
 }
