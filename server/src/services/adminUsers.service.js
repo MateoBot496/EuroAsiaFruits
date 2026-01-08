168b6c4ca144db79c0408aab7bc950cc4eec8369
Peilei Ye <peileiye@Peileis-MacBook-Air.local>
2026-01-08 16:00:24 +0100

    cambio bcryptjs por bcrypt

--- server/src/services/adminUsers.service.js
+++ server/src/services/adminUsers.service.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const pool = require('../config/db');
 const { findAdminByEmail, findAdminById } = require("./users.service.js");
 
