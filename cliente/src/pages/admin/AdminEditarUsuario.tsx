8c9946d12727fe7c9d21bc4c260ecea648a5d420
MateoBot496 <mateomontesacosta@gmail.com>
2026-02-22 21:28:21 +0100

    Cambios admin

---
+++ cliente/src/pages/admin/AdminEditarUsuario.tsx
@@ -0,0 +1,8 @@
+export default function AdminEditarUsuario() {
+    return (
+        <div>
+            <h1>Admin - Editar Usuario</h1>
+            {/* Aquí iría el formulario para editar un usuario */}
+        </div>
+    );
+}
---
+++ cliente/src/pages/admin/AdminTodosUsuarios.tsx
@@ -0,0 +1,8 @@
+export default function AdminTodosUsuarios() {
+    return (
+        <div>
+            <h1>Admin - Todos los Usuarios</h1>
+            {/* Aquí iría la tabla o lista de usuarios */}
+        </div>
+    );
+}
